3c70ed5403867ff3bcff7bac532f862745ef5485
mirrislegend <mirrislegend@gmail.com>
2014-08-07 16:59:25 -0400

    Nevermind. It wasn't anywhere near done. Still working on it. Some of the issues are really strange.

--- tcpServer.c
+++ tcpServer.c
@@ -11,24 +11,26 @@
 int main(int argc, char *argv[])
 {
 	//relay entries
-	typedef struct {
+	typedef struct relay_entry {
 		int position;
 		struct sockaddr_in relay_addr;
 		int port_number;
 		int occupied;
-	} relay_entry;
-	relay_entry relay_table[10];
+	} relay_table[10];
+	
 
 	//channels
-	typedef struct {
+	typedef struct channel{
 		char *channel_name;
-		relay_entry subscribers[10];
-	} channel;
-	channel channel_table[10];
+		struct relay_entry subscribers[10];
+	} channel_table[10];
 
-	void setUpRelayTable(relay_entry []);
-	void setUpChannelTable(channel[]);
+
+	void setUpRelayTable(struct relay_entry []);
+	void setUpChannelTable(struct channel[]);
 	void setUpServerSocket(int, char**);
+
+
 
 	//setting up
 	setUpRelayTable(relay_table);
@@ -39,7 +41,7 @@ int main(int argc, char *argv[])
 }
 
 //"relay entry" = "entry in relay table"
-void setUpRelayTable(relay_entry *table[])
+void setUpRelayTable(struct relay_entry *table[])
 {
 	int capacity = 10;
 	int n;
@@ -59,7 +61,7 @@ void setUpRelayTable(relay_entry *table[])
 	}
 }
 
-void setUpChannelTable(channel *table[])
+void setUpChannelTable(struct channel *table[])
 {
 	
 	//setting up the channel names
@@ -76,8 +78,10 @@ void setUpChannelTable(channel *table[])
 	//intializing subscriber relay entries
 	const int capacity = 10;
 	int i, j;
-	for (i = 0; i < capacity; ++i){
-		for (j = 0; j < capacity; ++j){	
+	for (i = 0; i < capacity; ++i)
+	{
+		for (j = 0; j < capacity; ++j)
+		{	
 			relay_entry * table_entry = &table[i].subscribers[j];
 			table_entry->occupied = 0;
 			table_entry->position = j;
@@ -114,7 +118,7 @@ void subscribe_to_channel(char *chann_req, struct sockaddr_in *subscriber_addr)
 */
 
 //add to relay table
-struct sockaddr_in add_to_relay(sockaddr_in *addr)
+struct sockaddr_in add_to_relay(struct sockaddr_in *addr)
 {
 	int j = 0;
 	while(relay_table[j].occupied==1){
@@ -133,7 +137,7 @@ struct sockaddr_in add_to_relay(sockaddr_in *addr)
 }
 
 
-void serve(int fd, sockaddr_in *addr)
+void serve(int fd, struct sockaddr_in *addr)
 {	
 	//setting up new socket for relay
 	int relay_socket;

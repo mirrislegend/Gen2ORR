23d0d8223f7c9532150ccf7d28053eaf49408e3b
mirrislegend <mirrislegend@gmail.com>
2014-08-29 18:01:00 -0400

    8/29 work server side has no errors. non functional, but no errors. it should be easy to get it running. gotta get client side in line with server side

--- tcpServer.c
+++ tcpServer.c
@@ -13,15 +13,20 @@ typedef struct{
 	char *channel_name;
 	int channelport;
 	struct sockaddr_in channel_addr;
+	int channelsocket;
 	char *chanbuff;
 	int numsub;
 	int subscriber[10]; //(file descriptors of sockets)
 } channel;
 
-channel channel_table[10];
+//channel table consists of channels
+//channel channel_table[10];
+//channel table[10];
 
 
+//takes in a table of channels. outputs a table of channels
 void setUpChannelTable(channel table[])
+//channel_table* setUpChannelTable(channel* table)
 {
 
 	int capacity = 10;
@@ -39,12 +44,38 @@ void setUpChannelTable(channel table[])
 	table[9].channel_name = "J";
 
 	//intializing channels
-	for (int i = 0; i < capacity; ++i)
+	for (int i = 0; i < capacity; ++i) //each channel has
 	{
-		table[i].channelport=3400+i;
-		table[i].chanbuff="";
-		table[i].numsub=0;
-		//and make a socket
+		table[i].channelport=3400+i; //a port number
+		table[i].chanbuff="";		//a buffer
+		table[i].numsub=0;		//a count of the number of subscribers
+
+		//a socket with an address		
+		if ((table[i].channelsocket=socket(AF_INET, SOCK_STREAM,0)) < 0)
+		{
+			perror("socket");
+			exit(1);
+		}
+
+		memset(&(table[i].channel_addr), 0, sizeof(table[i].channel_addr));
+		table[i].channel_addr.sin_family = AF_INET;
+		table[i].channel_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+		table[i].channel_addr.sin_port = htons(table[i].channelport);
+
+		if (bind(table[i].channelsocket, (struct sockaddr *)&(table[i].channel_addr), sizeof(table[i].channel_addr)) < 0)
+		{
+			perror("bind");
+			exit(1);
+		}
+
+		//put channel socket into listening mode
+		if (listen(table[i].channelsocket, 5)==-1)
+		{
+			perror("listen");
+			exit(1);
+		}
+
+		//initialize all subscribers to zero
 		for (int j; j<10; j++)
 		{
 			table[i].subscriber[j]=0;
@@ -83,9 +114,10 @@ void subscribe_to_channel(char *chann_req, struct sockaddr_in *subscriber_addr)
 //my subscribe to channel
 void subscribe_to_channel(channel c, int clsock)
 {
+	int x = htonl(c.channelport);
 
 	//send port number of channel to client
-	if (write(clsock, c.channelport, strlen(c.channelport))<0)
+	if (write(clsock, &x, sizeof(x))<0)
 	{
 		perror("write");
 		exit(1);
@@ -93,14 +125,16 @@ void subscribe_to_channel(channel c, int clsock)
 
 	close(clsock);
 
-	//accept client's connection to channel's port
+	//accept client's connection to channel's socket
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
 	clsock=accept(c.channelsocket, (struct  sockaddr *)&client_addr, &client_len);
 
 
 	int n=0;
 	while(1) //find an open slot
 	{
-		if (c.subscriber[n]=0)
+		if (c.subscriber[n]==0)
 		{
 			c.subscriber[n]=clsock;
 			break;
@@ -128,33 +162,25 @@ void channelserve(channel c)
 {
 	//entire channel socket is already in channel table
 
-
-	//put channel socket into listening mode
-	if (listen(c.chansocket, 5)==-1)
-	{
-		perror("listen");
-		exit(1);
-	}
-
 	int n=0;
 	while(1)
 	{	
 		
-		for (int i=0; i<n, i++) //for each member
+		for (int i=0; i<n; i++) //for each member
 		{
-			if(read(c.subscriber[i], c.chanbuff, strlen(c.chanbuff)<0) //read from that member
+			if(read(c.subscriber[i], c.chanbuff, strlen(c.chanbuff))<0) //read from that member
 			{
 				perror("read");
 				exit(1);
 			}
 
-			if(c.chanbuff!="") //if something is read in
+			if(strcmp(c.chanbuff,"") != 0) //if something is read in
 			{
-				for (int j=0; j<n, j++) //for each member
+				for (int j=0; j<n; j++) //for each member
 				{
 					if (j!=i) //that is not the current member
 					{
-						if (write(c.subscriber[j], chanbuff, strlen(chanbuff)<0) //write to that member
+						if (write(c.subscriber[j], c.chanbuff, strlen(c.chanbuff))<0) //write to that member
 						{
 							perror ("read");
 							exit(1);
@@ -177,20 +203,23 @@ int main(int argc, char const *argv[])
 {
 	//declaring variables and methods for later use
 	int lsock;
-	void setUpChannelTable(channel[]);
+	//channel table[];
+	channel table[10];
+	//void setUpChannelTable(channel variable[]);
+	//void subscribe_to_channel(channel c, int clientsocket);
 	
 
 	//setting up
-	setUpChannelTable(channel_table);
+	setUpChannelTable(&table[0]);
 	
 
 	//setting up our address
-	struct sockaddr_in my_addr;
-	memset(&my_addr, 0, sizeof(my_addr));
+	struct sockaddr_in rendez_addr;
+	memset(&rendez_addr, 0, sizeof(rendez_addr));
 
-	my_addr.sin_family = AF_INET;
-	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	my_addr.sin_port = htons(atoi(argv[1]));
+	rendez_addr.sin_family = AF_INET;
+	rendez_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	rendez_addr.sin_port = htons(atoi(argv[1]));
 
 	//error-handling
 	if (argc != 2)
@@ -207,7 +236,7 @@ int main(int argc, char const *argv[])
 	}
 
 	//binding address to socket
-	if(bind(lsock, (struct sockaddr *)&my_addr, sizeof(my_addr))==-1)
+	if(bind(lsock, (struct sockaddr *)&rendez_addr, sizeof(rendez_addr))==-1)
 	{
 		perror("bind");
 		exit(1);
@@ -234,44 +263,45 @@ int main(int argc, char const *argv[])
 
 		//acknowledgement
 		printf("Received connection from %s. Waiting to receive channel.\n", inet_ntoa(client_addr.sin_addr));
-/*
+
 		//get channel-desired name
-		char *buff;
+		char buff[256];
 		if (read(csock, buff, sizeof(buff))==-1)
 		{
 			perror("read");
 			exit(1);
 		}
-*/
 		
-
-		//int n
-		//while n<10
-			//if table[n].channel_name==buff
-				//break
-			//else
-				//n++
-
-			
-		
-
-		//channel table needs "# occupants" field		
-		//if table[n].occupants==0
-			//fork
-			switch (fork())
+		int n=0;
+		while (n<10)
+		{
+			if (table[n].channel_name==buff)
 			{
-				case -1:
-					perror("fork");
-					exit(1);
-				case 0:
-					channelserve(table[n]);	
-					//subscribe_to_channel(table[n], csock);
-				default:
-					//subscribe_to_channel(table[n], csock);
-					return 0;
-					break;
-
+				break;
 			}
+			else
+			{
+				n++;
+			}
+		}
+			
+		subscribe_to_channel(table[n], csock);
+			
+	
+		//fork
+		switch (fork())
+		{
+			case -1:
+				perror("fork");
+				exit(1);
+			case 0:
+				channelserve(table[n]);	
+			default:
+			
+				return 0;
+				break;
+
+		}
 		
 	}
 	return 0;
